bcad7f1d079836f8828af40cf70f61f6c0c2191e
madhulikaa223344 <madhulikaa223344@gmail.com>
2024-01-04 20:35:33 +0530

    added private router to profile page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import { FaSearch } from 'react-icons/fa';
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className='bg-slate-200 shadow-md'>
     <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -14,7 +15,7 @@ export default function Header() {
         </h1>
       </Link>
       <form
-       // onSubmit={handleSubmit}
+      //  onSubmit={handleSubmit}
         className='bg-slate-100 p-3 rounded-lg flex items-center'
       >
         <input
@@ -40,7 +41,7 @@ export default function Header() {
           </li>
         </Link>
         <Link to='/profile'>
-          {/* {currentUser ? (
+          {currentUser ? (
             <img
               className='rounded-full h-7 w-7 object-cover'
               src={currentUser.avatar}
@@ -48,7 +49,7 @@ export default function Header() {
             />
           ) : (
             <li className=' text-slate-700 hover:underline'> Sign in</li>
-          )} */}
+          )}
         </Link>
       </ul>
     </div>
