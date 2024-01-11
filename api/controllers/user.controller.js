d24f1a5cd92bbf9479aa840d2c89158de0b5e79b
madhulikaa223344 <madhulikaa223344@gmail.com>
2024-01-11 15:30:23 +0530

    complete show user listing functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
 import  errorHandler  from '../utills/error.js';
+import Listing from '../models/listing.model.js';
 
 
 
@@ -46,4 +47,19 @@ export const updateUser = async (req, res, next) => {
       next(error);
     }
   };
+
+  export const getUserListing=async (req, res, next) => {
+    if(req.user.id==req.params.id){
+      try{
+      const listings=await Listing.find({userRef:req.params.id})
+      res.status(200).json(listings);
+      }
+      catch(error){
+        next(error);
+      }
+    }
+    else{
+      return next(errorHandler(401,"You can only view your own Listings"));
+    }
+  }
   
