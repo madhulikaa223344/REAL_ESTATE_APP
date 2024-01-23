2ae8bc91db01cf4886d8206858bf0d869d0c2ca1
madhulikaa223344 <madhulikaa223344@gmail.com>
2024-01-24 00:20:47 +0530

    fix bug 1

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -5,7 +5,7 @@ import errorHandler from "../utills/error.js";
 
 
 export const signup = async (req, res, next) => {
-  console.log(req.body);
+  //console.log(req.body);
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
