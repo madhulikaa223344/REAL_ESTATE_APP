8e2ac6d181f66752850f580f0b15fcc7f1ea2e55
madhulikaa223344 <madhulikaa223344@gmail.com>
2024-01-24 12:30:57 +0530

    ready to deploy2

--- api/index.js
+++ api/index.js
@@ -1,17 +1,15 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import authRouter from './routes/auth.route.js';
+import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
-import cors from "cors";
-import path from "path";
+import path from 'path';
 dotenv.config();
 
-
 mongoose
-  .connect(process.env.MONGO ,{ useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO)
   .then(() => {
     console.log('Connected to MongoDB!');
   })
@@ -19,13 +17,22 @@ mongoose
     console.log(err);
   });
 
-  const __dirname=path.resolve();
+  const __dirname = path.resolve();
 
 const app = express();
-app.use(express.urlencoded({ extended: true }))
+
 app.use(express.json());
+
 app.use(cookieParser());
-app.use(cors());
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
@@ -33,21 +40,12 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
   });
-
-  app.use("/api/auth", authRouter);
-  app.use("/api/user", userRouter);
-  app.use("/api/listing", listingRouter);
-
-
-  app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-      success: false,
-      statusCode,
-      message,
-    });
-  });
+});
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -71,7 +71,7 @@ export default function SignIn() {
           <span className='text-blue-700'>Sign up</span>
         </Link>
       </div>
-      {error && <p className='text-red-500 mt-5'>{error}</p>}
+      {/* {error && <p className='text-red-500 mt-5'>{error}</p>} */}
     </div>
   );
 }
